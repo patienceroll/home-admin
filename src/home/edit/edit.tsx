76452cf9316982a9477f8cef92b14381e51def03
pactience <1079105171@qq.com>
2021-03-21 14:45:25 +0800

    完成home项目编辑的回填

--- src/home/edit/edit.tsx
+++ src/home/edit/edit.tsx
@@ -6,18 +6,27 @@ import {
   ResourceProps,
   SimpleForm,
   TextInput,
-  useEditContext,
 } from "react-admin";
 
 const EditHomeItem: ResourceProps["edit"] = (props) => {
-  const ctx = useEditContext(props);
   return (
     <Edit {...props} title="编辑">
       <SimpleForm>
         <TextInput source="title" required label="标题" />
         <TextInput source="subTitle" label="子标题" />
         <TextInput source="url" required label="地址" />
-        <ImageInput source="image" required label="封面">
+        <ImageInput
+          source="image"
+          required
+          label="封面"
+          accept=".png,.jpg,.gif"
+          placeholder={
+            <div>
+              <p>请上传图片</p>
+              <p>支持.png,.jpg,.gif格式</p>
+            </div>
+          }
+        >
           <ImageField />
         </ImageInput>
       </SimpleForm>
--- src/post/getOne.ts
+++ src/post/getOne.ts
@@ -14,7 +14,6 @@ const getOne: DataProvider["getOne"] = (resourse, params) => {
           url,
           image: await newFile(image),
         };
-        console.log(data);
         return { data } as any;
       });
     default:
