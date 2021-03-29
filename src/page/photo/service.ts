2a98f831ecc266a6fa474a78d4a64669a7e9bbc3
pactience <1079105171@qq.com>
2021-03-29 14:36:23 +0800

    docs: ✏️ doc
    doc

--- src/page/photo/service.ts
+++ src/page/photo/service.ts
@@ -1,5 +1,9 @@
 import { Get } from "../../fetch/fetch";
+import type { ListType } from "../../fetch/data";
 
-const GetPhotoList = () => {
-  return Get("/photo");
+import type Data from "./data";
+
+/** 获取相册列表 */
+export const GetPhotoList = (params: Data.PhotoListParams) => {
+  return Get<ListType<Data.PhotoListItem>>("/photo", params);
 };
